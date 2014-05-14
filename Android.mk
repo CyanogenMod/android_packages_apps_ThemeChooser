6f8b9883b3372d049ad960538b938a559582c0e3
Thomas Wendt <thoemy@gmx.net>
2014-05-14 17:26:11 +0200

    Move checkboxes for applied components under a 'Applied' header
    Makes it clearer that the components are already applied.
    
    Change-Id: If41e6eee30671aa3b49314a28681731b989ff248

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_PACKAGE_NAME := ThemeChooser
+LOCAL_PACKAGE_NAME := ThemeChooserOss
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
