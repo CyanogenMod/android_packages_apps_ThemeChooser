09b5b00e2f8952630f799cef5854b87ca223eea9
Clark Scheff <clark@cyngn.com>
2014-08-25 09:38:26 -0700

    Collapse notifications when multiple themes installed
    Rather than flood the users notification drawer with every theme
    installed, we can consolidate theme into one notification if more
    than one theme was recently installed.  This will come in handy
    when people restore backups from google play and all their themes
    are installed.
    
    Change-Id: I00c676769115a1cf44b22a138b32287c3794b9ca

--- src/com/cyngn/theme/util/NotificationHelper.java
+++ src/com/cyngn/theme/util/NotificationHelper.java
@@ -22,6 +22,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.graphics.BitmapFactory;
 import android.text.TextUtils;
 
@@ -29,6 +30,8 @@ import com.cyngn.theme.chooser.ChooserActivity;
 import com.cyngn.theme.chooser.R;
 
 public class NotificationHelper {
+    private static final int NOTIFICATION_ID = 0x434D5443;
+
     public static void postThemeInstalledNotification(Context context, String pkgName) {
         String themeName = null;
         try {
@@ -46,27 +49,44 @@ public class NotificationHelper {
             return;
         }
 
+        int themeCount = PreferenceUtils.getNewlyInstalledThemeCount(context) + 1;
+
         Intent intent = new Intent(context, ChooserActivity.class);
         intent.setAction(Intent.ACTION_MAIN);
         intent.putExtra("pkgName", pkgName);
         PendingIntent pi = PendingIntent.getActivity(context, 0, intent,
                 PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_CANCEL_CURRENT);
 
+        String title = null;
+        String content = null;
+        final Resources res = context.getResources();
+        if (themeCount == 1) {
+            title = String.format(res.getString(
+                    R.string.theme_installed_notification_title), themeName);
+            content = res.getString(R.string.theme_installed_notification_text);
+        } else {
+            title = String.format(res.getString(R.string.themes_installed_notification_title),
+                    themeCount);
+            content = String.format(res.getQuantityString(
+                            R.plurals.themes_installed_notification_text, themeCount -1),
+                            themeName, themeCount - 1);
+        }
         NotificationManager nm =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         Notification notice = new Notification.Builder(context)
                 .setAutoCancel(true)
                 .setOngoing(false)
-                .setContentTitle(String.format(
-                        context.getString(R.string.theme_installed_notification_title), themeName))
-                .setContentText(context.getString(R.string.theme_installed_notification_text))
+                .setContentTitle(title)
+                .setContentText(content)
                 .setContentIntent(pi)
                 .setSmallIcon(R.drawable.ic_notifiy)
                 .setLargeIcon(BitmapFactory.decodeResource(context.getResources(),
                         R.drawable.ic_app_themes))
                 .setWhen(System.currentTimeMillis())
                 .build();
-        nm.notify(pkgName.hashCode(), notice);
+        if (themeCount > 1) notice.number = themeCount;
+        nm.notify(NOTIFICATION_ID, notice);
+        PreferenceUtils.setNewlyInstalledThemeCount(context, themeCount);
     }
 
     public static void cancelNotificationForPackage(Context context, String pkgName) {
