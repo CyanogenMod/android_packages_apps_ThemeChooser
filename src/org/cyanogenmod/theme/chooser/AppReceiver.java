73cf15260ac4af04070662bc1d21161677be7dce
d34d <clark@cyngn.com>
2015-03-02 15:41:16 -0800

    Themes: Update ThemesContract with install state [3/3]
    Change-Id: I821117c75fd6fdb8684e1c95ce6f83bfd6a8f192

--- src/org/cyanogenmod/theme/chooser/AppReceiver.java
+++ src/org/cyanogenmod/theme/chooser/AppReceiver.java
@@ -18,13 +18,9 @@ package org.cyanogenmod.theme.chooser;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.content.res.ThemeManager;
 import android.net.Uri;
+import android.provider.ThemesContract;
 import org.cyanogenmod.theme.util.NotificationHelper;
-import org.cyanogenmod.theme.util.PreferenceUtils;
 
 import java.util.Set;
 
@@ -35,44 +31,11 @@ public class AppReceiver extends BroadcastReceiver {
         Uri uri = intent.getData();
         String pkgName = uri != null ? uri.getSchemeSpecificPart() : null;
         String action = intent.getAction();
-        boolean isReplacing = intent.getExtras().getBoolean(Intent.EXTRA_REPLACING, false);
 
-        if (Intent.ACTION_PACKAGE_ADDED.equals(action) && !isReplacing) {
-            if (!isThemeBeingProcessed(context, pkgName)) {
-                NotificationHelper.postThemeInstalledNotification(context, pkgName);
-            } else {
-                // store this package name so we know it's being processed
-                PreferenceUtils.addThemeBeingProcessed(context, pkgName);
-            }
-        } else if (Intent.ACTION_PACKAGE_FULLY_REMOVED.equals(action)) {
+        if (ThemesContract.Intent.ACTION_THEME_INSTALLED.equals(action)) {
+            NotificationHelper.postThemeInstalledNotification(context, pkgName);
+        } else if (ThemesContract.Intent.ACTION_THEME_REMOVED.equals(action)) {
             NotificationHelper.cancelNotificationForPackage(context, pkgName);
-        } else if (Intent.ACTION_THEME_RESOURCES_CACHED.equals(action)) {
-            final String themePkgName = intent.getStringExtra(Intent.EXTRA_THEME_PACKAGE_NAME);
-            final int result = intent.getIntExtra(Intent.EXTRA_THEME_RESULT,
-                    PackageManager.INSTALL_FAILED_THEME_UNKNOWN_ERROR);
-            Set<String> processingThemes =
-                    PreferenceUtils.getInstalledThemesBeingProcessed(context);
-            if (processingThemes != null &&
-                    processingThemes.contains(themePkgName) && result >= 0) {
-                NotificationHelper.postThemeInstalledNotification(context, themePkgName);
-                PreferenceUtils.removeThemeBeingProcessed(context, themePkgName);
-            }
         }
-    }
-
-    private boolean isTheme(Context context, String pkgName) throws NameNotFoundException {
-        PackageInfo pi = context.getPackageManager().getPackageInfo(pkgName, 0);
-        if (pi == null) return false;
-
-        if (pi.themeInfo != null) {
-            return true;
-        }
-
-        return false;
-    }
-
-    private boolean isThemeBeingProcessed(Context context, String pkgName) {
-        ThemeManager tm = (ThemeManager) context.getSystemService(Context.THEME_SERVICE);
-        return tm.isThemeBeingProcessed(pkgName);
     }
 }
