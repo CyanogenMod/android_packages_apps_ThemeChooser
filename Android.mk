e91e3348627f552a531d60dff8aac0da95259974
d34d <clark@cyngn.com>
2015-05-05 06:23:42 -0700

    Obfuscate das Chooser
    Change-Id: I1c18473dd7bb86d657308681ab1c663ed23e32f8

--- Android.mk
+++ Android.mk
@@ -5,6 +5,13 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_MODULE_TAGS := optional
 
+# Obfuscate user builds, eng and userdebug will remain unobfuscated
+ifeq ($(TARGET_BUILD_VARIANT),user)
+LOCAL_PROGUARD_ENABLED := obfuscation
+endif
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
 LOCAL_PACKAGE_NAME := ThemeChooser
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
