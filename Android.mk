294a3645905c5e0138c92033abd3b8a3426e1bc1
jrizzoli <joey@cyanogenmoditalia.it>
2016-01-25 22:12:36 +0100

    ThemeChooser: update UI
    - Use animated FAB to apply themes instead of animated button
    - Auto-Check all components when user opens a theme preview (as requested by Dave)
    - Add banner to navigation drawer (by Dave and Asher)
    - Vibrant color for theme cards (from default wallpaper)
    - Update toolbar, FAB and checkboxes color to match wallpaper vibrant color
    - AVD sound preview
    
    Change-Id: I7f4140997fea074cf6cccdc6e580cc2bbe4cf1a8
    Signed-off-by: jrizzoli <joey@cyanogenmoditalia.it>

--- Android.mk
+++ Android.mk
@@ -9,8 +9,17 @@ LOCAL_PACKAGE_NAME := ThemeChooser
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
+LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, res) \
+    $(TOP)/frameworks/support/v7/cardview/res
+
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v4 \
-    org.cyanogenmod.platform.internal
+    org.cyanogenmod.platform.internal \
+    android-support-v7-palette \
+    android-support-v7-cardview
+
+LOCAL_AAPT_FLAGS := \
+    --auto-add-overlay \
+    --extra-packages android.support.v7.cardview
 
 include $(BUILD_PACKAGE)
