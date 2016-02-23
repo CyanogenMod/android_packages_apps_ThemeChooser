de9323a0023787fa307975153eadf7868ebde177
d34d <clark@cyngn.com>
2016-02-23 14:03:44 -0800

    Themes: Refactor themes to CMSDK [5/6]
    Change-Id: Ifafc899f309c91806296641e4d1ef083058e20b4
    TICKET: CYNGNOS-2126

--- src/org/cyanogenmod/theme/chooser/AppReceiver.java
+++ src/org/cyanogenmod/theme/chooser/AppReceiver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 The CyanogenMod Project
+ * Copyright (C) 2014-2016 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,10 +19,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.provider.ThemesContract;
 import org.cyanogenmod.theme.util.NotificationHelper;
-
-import java.util.Set;
 
 public class AppReceiver extends BroadcastReceiver {
 
@@ -32,9 +29,9 @@ public class AppReceiver extends BroadcastReceiver {
         String pkgName = uri != null ? uri.getSchemeSpecificPart() : null;
         String action = intent.getAction();
 
-        if (ThemesContract.Intent.ACTION_THEME_INSTALLED.equals(action)) {
+        if (cyanogenmod.content.Intent.ACTION_THEME_INSTALLED.equals(action)) {
             NotificationHelper.postThemeInstalledNotification(context, pkgName);
-        } else if (ThemesContract.Intent.ACTION_THEME_REMOVED.equals(action)) {
+        } else if (cyanogenmod.content.Intent.ACTION_THEME_REMOVED.equals(action)) {
             NotificationHelper.cancelNotificationForPackage(context, pkgName);
         }
     }
